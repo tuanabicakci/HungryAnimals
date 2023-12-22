a9ca2498b3c2183d47a16fd7674c2b8be99781d7
tuanabicakci <bicakcituana@gmail.com>
2023-12-22 20:38:13 +0300

    Collision decisions

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,21 +7,25 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     public float spawnRangeX = 15;
     public float spawnPosZ = 25;
+    public float startDelay = 2f;
+    public float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal",startDelay,spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    void SpawnRandomAnimal()
+    {
         int animalIndex = Random.Range(0,animalPrefabs.Length);
         Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),0,spawnPosZ);
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
-        }
+        Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
     }
 }
